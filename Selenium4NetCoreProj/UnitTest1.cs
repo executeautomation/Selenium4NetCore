77a0e95a9b2ec8cdf281a4d95a2b5936c824a3d0
Karthik <karthik@techgeek.co.in>
2021-05-01 13:52:30 +1200

    Updated code with Selenium 4 Beta 2 updates

--- Selenium4NetCoreProj/UnitTest1.cs
+++ Selenium4NetCoreProj/UnitTest1.cs
@@ -2,6 +2,13 @@ using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.DevTools;
+using System;
+using System.Threading;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+using DevToolsSessionDomains = OpenQA.Selenium.DevTools.V89.DevToolsSessionDomains;
+using Network = OpenQA.Selenium.DevTools.V89.Network;
+
 
 /// <summary>
 /// Author: Karthik KK
@@ -12,19 +19,25 @@ namespace Selenium4NetCoreProj
     public class Tests
     {
 
-        protected DevToolsSession session;
+        protected IDevToolsSession session;
         protected IWebDriver driver;
+        protected DevToolsSessionDomains devToolsSession;
+
 
         [SetUp]
         public void Setup()
         {
+            new DriverManager().SetUpDriver(new ChromeConfig());
 
             //Set ChromeDriver
             driver = new ChromeDriver();
             //Get DevTools
             IDevTools devTools = driver as IDevTools;
             //DevTools Session
-            session = devTools.CreateDevToolsSession();
+            session = devTools.GetDevToolsSession();
+
+            devToolsSession = session.GetVersionSpecificDomains<DevToolsSessionDomains>();
+            devToolsSession.Network.Enable(new Network.EnableCommandSettings());
         }
 
         /// <summary>
@@ -33,14 +46,48 @@ namespace Selenium4NetCoreProj
         [Test]
         public void NetworkInterception()
         {
-            session.Network.Enable(new OpenQA.Selenium.DevTools.Network.EnableCommandSettings());
-
-            session.Network.SetBlockedURLs(new OpenQA.Selenium.DevTools.Network.SetBlockedURLsCommandSettings()
+            devToolsSession.Network.SetBlockedURLs(new Network.SetBlockedURLsCommandSettings()
             {
                 Urls = new string[] { "*://*/*.css", "*://*/*.jpg", "*://*/*.png" }
             });
 
-            driver.Url = "http://www.executeautomation.com";
+            driver.Url = "https://www.executeautomation.com";
+        }
+
+
+        [Test]
+        public void SetAddionalHeaders()
+        {
+            var extraHeader = new Network.Headers();
+            extraHeader.Add("headerName", "executeHacked");
+            devToolsSession.Network.SetExtraHTTPHeaders(new Network.SetExtraHTTPHeadersCommandSettings()
+            {
+                Headers = extraHeader
+            });
+
+            driver.Url = "https://www.executeautomation.com";
+        }
+
+        [Test]
+        public void SetUserAgent()
+        {
+            devToolsSession.Network.SetUserAgentOverride(new Network.SetUserAgentOverrideCommandSettings()
+            {
+                UserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko)"
+            });
+
+            ManualResetEventSlim requestSync = new ManualResetEventSlim(false);
+            EventHandler<Network.RequestWillBeSentEventArgs> requestWillBeSentHandler = (sender, e) =>
+            {
+                Assert.That(e.Request.Headers["User-Agent"], Does.Contain("CPU iPhone OS"));
+                requestSync.Set();
+            };
+
+            devToolsSession.Network.RequestWillBeSent += requestWillBeSentHandler;
+
+            driver.Url = "https://www.executeautomation.com";
+            requestSync.Wait(TimeSpan.FromSeconds(6));
+
         }
     }
 }
